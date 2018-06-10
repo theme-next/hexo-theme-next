8e46db00393cbc4692929100a097a104c97fb494
Ivan.Nginx <ivan.nginx@gmail.com>
2018-06-10 21:36:30 +0300

    Refacored `label.js`.

--- scripts/tags/label.js
+++ scripts/tags/label.js
@@ -9,9 +9,11 @@
  *           If not defined, default class will be selected.
  */
 
+/* global hexo */
+
 'use strict';
 
-function postLabel (args) {
+function postLabel(args) {
   args = args.join(' ').split('@');
   var classes = args[0] || 'default';
   var text = args[1] || '';
