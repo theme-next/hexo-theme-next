6470e266070c8072e9ceabee0d75b900ccff579e
Ivan.Nginx <ivan.nginx@gmail.com>
2018-06-10 22:45:45 +0300

    Refactored `center-quote.js`.

--- scripts/tags/center-quote.js
+++ scripts/tags/center-quote.js
@@ -7,12 +7,14 @@
  * {% cq %}Something{% endcq %}
  */
 
+/* global hexo */
+
 'use strict';
 
-function centerQuote (args, content) {
-  return '<blockquote class="blockquote-center">' +
-            hexo.render.renderSync({text: content, engine: 'markdown'}) +
-          '</blockquote>';
+function centerQuote(args, content) {
+  return '<blockquote class="blockquote-center">'
+       + hexo.render.renderSync({text: content, engine: 'markdown'})
+       + '</blockquote>';
 }
 
 hexo.extend.tag.register('centerquote', centerQuote, {ends: true});
