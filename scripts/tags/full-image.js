345c72d5bfc0bfa67d99505b321b0e4d4b964611
Ivan.Nginx <ivan.nginx@gmail.com>
2018-06-11 14:44:35 +0300

    Refactored `full-image.js`. [2]

--- scripts/tags/full-image.js
+++ scripts/tags/full-image.js
@@ -14,14 +14,12 @@
 function fullImage(args) {
   args = args.join(' ').split(',');
   var src = args[0];
-  var alt = args[1] || '';
-  var title = args[2] || '';
+  var alt = args[1].trim() || '';
+  var title = args[2].trim() || '';
 
   if (!src) {
     hexo.log.warn('Image src can NOT be empty');
   }
-  alt = alt.trim();
-  title = title.trim();
 
   var image = ['<span itemprop="image" itemscope itemtype="http://schema.org/ImageObject"><img itemprop="url image" src="' + src + '" class="full-image"'];
   alt.length > 0 && image.push('alt="' + alt + '"');
