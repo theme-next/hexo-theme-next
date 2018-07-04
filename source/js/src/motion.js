b378211e6558aeea75e387a13fb9e6b28e99593c
Ivan.Nginx <ivan.nginx@gmail.com>
2018-07-04 20:59:48 +0300

    Custom sidebar width & content width for all schemes. (#339)
    * Custom sidebar width & content width for all schemes.
    
    1. [BR]: Removed `max_content_width` setting (which was only for Gemini scheme).
    2. [BR]: renamed `sidebar_width` to `sidebar.width` option (moved from global to section to section under `sidebar`).
    3. Added `desktop-largest()` condition and setted to >1600px. Old `desktop-large()` now redefined to >1200px. Added new `$content-desktop-largest` variable.
    4. Replaced `$main-desktop` & `$main-desktop-large` variables with standard `$content-desktop` & `$content-desktop-large` for Pisces/Gemini schemes.
    5. Moved `$sidebar-desktop` variable from global `base.styl` variables file to `Pisces.styl` (because it's only for Pisces/Gemini).
    6. Moved layout global styles variables from `Gemini.styl` to `Pisces.styl`.

--- source/js/src/motion.js
+++ source/js/src/motion.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
   sidebarToggleLines.push(sidebarToggleLine2nd);
   sidebarToggleLines.push(sidebarToggleLine3rd);
 
-  var SIDEBAR_WIDTH = '320px';
+  var SIDEBAR_WIDTH = CONFIG.sidebar.width ? CONFIG.sidebar.width : '320px';
   var SIDEBAR_DISPLAY_DURATION = 200;
   var xPos, yPos;
 
