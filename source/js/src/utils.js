fda59c09de4bd1466fb4e9c068d5ca51a08872d9
StevenJoeZhang <1119186082@qq.com>
2019-03-06 12:53:33 +0800

    Update

--- source/js/src/utils.js
+++ source/js/src/utils.js
@@ -273,7 +273,7 @@ NexT.utils = NexT.$u = {
   },
 
   getSidebarb2tHeight: function() {
-    var sidebarb2tHeight = (CONFIG.back2top.enable && CONFIG.back2top.sidebar) ? $('.back-to-top').height() : 0;
+    var sidebarb2tHeight = (CONFIG.back2top && CONFIG.back2top_sidebar) ? $('.back-to-top').height() : 0;
     return sidebarb2tHeight;
   },
 
