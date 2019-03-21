0b2012f796d827c98cb3c03a1fa6f953790c97d8
kor-coeo <kor.coeo@gmail.com>
2019-03-21 18:29:29 +0800

    click event

--- source/js/src/next-boot.js
+++ source/js/src/next-boot.js
@@ -73,3 +73,25 @@ if (window.CONFIG.dyfavicon.visibilitychange) {
   });
 
 }
+
+jQuery(document).ready(function($){
+  $("html,body").click(function(e){
+    var n=Math.round(Math.random()*100);
+    var $i=$("<b>").text("+"+n);
+    var x=e.pageX,y=e.pageY;
+    $i.css({
+      "z-index":99999,
+      "top":y-20,
+      "left":x,
+      "position":"absolute",
+      "color":"#E94F06"
+    });
+    $("body").append($i);
+    $i.animate(
+      {"top":y-180,"opacity":0},
+      1500,
+      function(){$i.remove();}
+    );
+    e.stopPropagation();
+  });
+});
