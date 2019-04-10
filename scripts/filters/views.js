e015d2c16d1c5ccfc5e67a4e8863db4972821c7f
MrTT <jiang.taojie@foxmail.com>
2019-04-11 00:42:19 +0800

    Fix QA

--- scripts/filters/views.js
+++ scripts/filters/views.js
@@ -2,19 +2,19 @@
 
 'use strict';
 
-hexo.extend.filter.register('before_generate', function(data) {
-    setEmptyCommentsViews('count.swig');
-    setEmptyCommentsViews('index.swig');
-});
-
 function setEmptyCommentsViews(fileName) {
-    let config = hexo.theme.config.comments;
-    if (!config.type) {
-        return;
-    }
-    let viewPath = `_third-party/comments/${config.type}/${fileName}`
-    let view = hexo.theme.getView(`_third-party/comments/${config.type}/${fileName}`);
-    if (!view) {
-        hexo.theme.setView(viewPath, '');
-    }
+  let config = hexo.theme.config.comments;
+  if (!config.type) {
+    return;
+  }
+  let viewPath = `_third-party/comments/${config.type}/${fileName}`;
+  let view = hexo.theme.getView(`_third-party/comments/${config.type}/${fileName}`);
+  if (!view) {
+    hexo.theme.setView(viewPath, '');
+  }
 }
+
+hexo.extend.filter.register('before_generate', function() {
+  setEmptyCommentsViews('count.swig');
+  setEmptyCommentsViews('index.swig');
+});
