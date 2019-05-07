1840a3ac2e40afcc00d5ef5fa5a48f0969b057d6
jiangtj <116749895@qq.com>
2019-05-07 18:35:49 +0800

    Fix QA

--- scripts/filters/injects.js
+++ scripts/filters/injects.js
@@ -1,19 +1,21 @@
+/* global hexo */
+
 'use strict';
 
 const fs = require('fs');
 
 class Inject {
-  constructor(){
+  constructor() {
     this.raws = [];
   }
-  raw(name, raw, ...args){
+  raw(name, raw, ...args) {
     this.raws.push({
       name,
       raw,
       args
     });
   }
-  file(name, file, ...args){
+  file(name, file, ...args) {
     this.raws.push({
       name,
       raw: fs.readFileSync(file).toString(),
@@ -22,26 +24,26 @@ class Inject {
   }
 }
 
-const injectType = ['head', 'header', 'sidebar']
+const injectType = ['head', 'header', 'sidebar'];
 
-const injects = {}
-injectType.forEach(item => {
+const injects = {};
+injectType.forEach((item) => {
   injects[item] = new Inject();
-})
+});
 
 hexo.on('generateBefore', function() {
 
   hexo.execFilterSync('theme_inject', injects);
   hexo.theme.config.injects = {};
-  injectType.forEach(type => {
+  injectType.forEach((type) => {
     hexo.theme.config.injects[type] = {};
-    injects[type].raws.forEach(injectObj => {
+    injects[type].raws.forEach((injectObj) => {
       let viewName = `inject/${type}/${injectObj.name}.swig`;
       hexo.theme.setView(viewName, injectObj.raw);
-      hexo.theme.config.injects[type][injectObj.name]={
+      hexo.theme.config.injects[type][injectObj.name] = {
         layout: viewName,
         locals: injectObj.args[0],
-        options: injectObj.args[1],
+        options: injectObj.args[1]
       };
     });
   });
