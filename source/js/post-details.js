d39c7e73d9a9803c79898682a054437f6c6c65d0
StevenJoeZhang <1119186082@qq.com>
2019-07-08 06:30:22 +0800

    back

--- source/js/post-details.js
+++ source/js/post-details.js
@@ -45,16 +45,23 @@ $(document).ready(function() {
     var currentTarget = $('.' + activePanelClassName);
     var target = $('.' + item.data('target'));
 
-    currentTarget.animate({ opacity: 0 }, TAB_ANIMATE_DURATION, function() {
-      currentTarget.hide();
-      target
-        .stop()
-        .css({'opacity': 0, 'display': 'block'})
-        .animate({ opacity: 1 }, TAB_ANIMATE_DURATION, function() {
-          currentTarget.removeClass(activePanelClassName);
-          target.addClass(activePanelClassName);
-        });
-    });
+    $.isFunction(html.velocity)
+      ? currentTarget.velocity('transition.slideUpOut', TAB_ANIMATE_DURATION, function() {
+        target
+          .velocity('stop')
+          .velocity('transition.slideDownIn', TAB_ANIMATE_DURATION)
+          .addClass(activePanelClassName);
+      })
+      : currentTarget.animate({ opacity: 0 }, TAB_ANIMATE_DURATION, function() {
+        currentTarget.hide();
+        target
+          .stop()
+          .css({'opacity': 0, 'display': 'block'})
+          .animate({ opacity: 1 }, TAB_ANIMATE_DURATION, function() {
+            currentTarget.removeClass(activePanelClassName);
+            target.addClass(activePanelClassName);
+          });
+      });
 
     item.siblings().removeClass(activeTabClassName);
     item.addClass(activeTabClassName);
