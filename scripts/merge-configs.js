977ff8dc1e60b63507e975168d5c35e27bcdc239
jiangtj <116749895@qq.com>
2019-07-09 19:36:19 +0800

    Fix QA

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -49,14 +49,14 @@ hexo.on('generateBefore', function() {
     if (tag.length) {
       visibleTags += 1;
     }
-  })
+  });
   hexo.theme.config.visibleTags = visibleTags;
   let visibleCategories = 0;
   hexo.locals.get('categories').forEach((categorie) => {
     if (categorie.length) {
       visibleCategories += 1;
     }
-  })
+  });
   hexo.theme.config.visibleCategories = visibleCategories;
 
 });
