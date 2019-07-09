95eb776e6166ff70e13425fcbaeb45ba9d6b295e
StevenJoeZhang <1119186082@qq.com>
2019-07-09 15:35:13 +0800

    next-boot

--- source/js/next-boot.js
+++ source/js/next-boot.js
@@ -41,4 +41,44 @@ $(document).ready(function() {
   // Bootstrap Motion.
   CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
 
+  /**
+   * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
+   * Need for Sidebar/TOC inner scrolling if content taller then viewport.
+   */
+  function updateSidebarHeight(height) {
+    height = height || 'auto';
+    $('.site-overview, .post-toc').css('max-height', height);
+  }
+
+  function initSidebarDimension() {
+    var updateSidebarHeightTimer;
+
+    $(window).on('resize', function() {
+      updateSidebarHeightTimer && clearTimeout(updateSidebarHeightTimer);
+
+      updateSidebarHeightTimer = setTimeout(function() {
+        var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
+
+        updateSidebarHeight(sidebarWrapperHeight);
+      }, 0);
+    });
+
+    // Initialize Sidebar & TOC Width.
+    var scrollbarWidth = NexT.utils.getScrollbarWidth();
+    if ($('.site-overview-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
+      $('.site-overview').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
+    }
+    if ($('.post-toc-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
+      $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
+    }
+
+    // Initialize Sidebar & TOC Height.
+    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
+  }
+  initSidebarDimension();
+
+  function wrapTable() {
+    $('table').not('.gist table').wrap('<div class="table-container"></div>');
+  }
+  wrapTable();
 });
