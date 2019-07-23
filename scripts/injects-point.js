a36be60772a1c75282283384348968679ddc93bd
jiangtj <116749895@qq.com>
2019-07-23 19:04:25 +0800

    Refactor comments

--- scripts/injects-point.js
+++ scripts/injects-point.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  views: ['head', 'header', 'sidebar', 'postMeta', 'postBodyEnd', 'footer', 'bodyEnd'],
+  views: ['head', 'header', 'sidebar', 'postMeta', 'postBodyEnd', 'footer', 'bodyEnd', 'comment'],
   styles: ['variable', 'mixin', 'style']
 };
