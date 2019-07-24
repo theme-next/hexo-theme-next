4fe694c05f9834597f846365c89bcfc2190b4a33
1v9 <mkb@live.cn>
2019-07-24 16:48:54 +0800

    🚀 bye bye string

--- scripts/filters/minify.js
+++ scripts/filters/minify.js
@@ -4,59 +4,46 @@
 
 hexo.extend.filter.register('after_generate', () => {
   const theme = hexo.theme.config;
-  const lists = hexo.route.list();
+  if (!theme.minify) return;
 
-  let scheme = theme.scheme;
-  if (scheme === 'Muse' || scheme === 'Mist') {
+  const lists = hexo.route.list();
+  const velocity = lists.filter(list => list.includes('lib/velocity'));
+  const fontawesome = lists.filter(list => list.includes('lib/font-awesome'));
+
+  if (!theme.motion.enable) {
+    hexo.route.remove('js/motion.js');
+    velocity.forEach(path => {
+      hexo.route.remove(path);
+    });
+  }
+
+  if (theme.vendors.fontawesome) {
+    fontawesome.forEach(path => {
+      hexo.route.remove(path);
+    });
+  }
+
+  if (!theme.exturl) {
+    hexo.route.remove('js/exturl.js');
+  }
+
+  if (!theme.save_scroll){
+    hexo.route.remove('js/js.cookie.js');
+    hexo.route.remove('js/scroll-cookie.js');
+  }
+
+  if (theme.vendors.jquery) {
+    hexo.route.remove('lib/jquery/index.js');
+  }
+
+  if (!theme.algolia_search.enable){
+    hexo.route.remove('js/algolia-search.js');
+  }
+
+  if (theme.scheme === 'Muse' || theme.scheme === 'Mist') {
     hexo.route.remove('js/affix.js');
-    hexo.route.remove('js/schemes/pisces.js');
-  } else if (scheme === 'Pisces' || scheme === 'Gemini') {
+    hexo.route.remove('js/theme.schemes/pisces.js');
+  } else if (theme.scheme === 'Pisces' || theme.scheme === 'Gemini') {
     hexo.route.remove('js/schemes/muse.js');
   }
-
-  let exturl = [
-    theme.exturl.toString(),
-    'js/exturl.js'
-  ];
-  let motion = [
-    theme.motion.enable.toString(),
-    'js/motion.js'
-  ];
-  motion = motion.concat(lists.filter(list => list.includes('lib/velocity')));
-  let algolia = [
-    theme.algolia_search.enable.toString(),
-    'js/algolia-search.js'
-  ];
-  let scroll = [
-    theme.save_scroll.toString(),
-    'js/js.cookie.js',
-    'js/scroll-cookie.js'
-  ];
-  let jquery;
-  if (theme.vendors.jquery) {
-    jquery = [theme.vendors.jquery.toString()].concat('lib/jquery/index.js');
-  } else {
-    jquery = 'true';
-  }
-  let fontawesome;
-  if (theme.vendors.fontawesome) {
-    fontawesome = [theme.vendors.fontawesome.toString()].concat(lists.filter(list => list.includes('lib/font-awesome')));
-  } else {
-    fontawesome = 'true';
-  }
-
-  const filter = option => {
-    if (option.includes('false') || option[0].includes('//')) {
-      option.slice(1).forEach(path => {
-        hexo.route.remove(path);
-      });
-    }
-  };
-
-  filter(exturl);
-  filter(motion);
-  filter(algolia);
-  filter(scroll);
-  filter(jquery);
-  filter(fontawesome);
 });
