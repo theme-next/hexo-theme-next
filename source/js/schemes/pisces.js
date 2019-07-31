e7bfe3a6f2272a70dd48fc6e11e80a8595d42fc2
StevenJoeZhang <1119186082@qq.com>
2019-07-31 14:50:30 +0800

    Fix affix bug

--- source/js/schemes/pisces.js
+++ source/js/schemes/pisces.js
@@ -20,19 +20,14 @@ $(document).ready(function() {
   function initAffix() {
     var headerOffset = getHeaderOffset();
     var footerOffset = getFooterOffset();
-    var sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight();
-    var contentHeight = $('#content').height();
 
-    // Not affix if sidebar taller than content (to prevent bottom jumping).
-    if (headerOffset + sidebarHeight < contentHeight) {
-      sidebarInner.affix({
-        offset: {
-          top   : headerOffset - sidebarOffset,
-          bottom: footerOffset
-        }
-      });
-      sidebarInner.affix('checkPosition');
-    }
+    sidebarInner.affix({
+      offset: {
+        top   : headerOffset - sidebarOffset,
+        bottom: footerOffset
+      }
+    });
+    sidebarInner.affix('checkPosition');
 
     $('#sidebar').css({ 'margin-top': headerOffset, 'margin-left': 'auto' });
   }
