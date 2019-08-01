34d758cbf4d76c4bd9282284391e2cc981c7f3f5
StevenJoeZhang <1119186082@qq.com>
2019-08-01 13:23:43 +0800

    Fix

--- scripts/filters/lazyload.js
+++ scripts/filters/lazyload.js
@@ -12,13 +12,12 @@ hexo.extend.filter.register('after_post_render', data => {
   if (!cheerio) cheerio = require('cheerio');
 
   const $ = cheerio.load(data.content, {decodeEntities: false});
-
   const images = $('img');
   if (!images.length) return data;
 
-  Array.from(images).forEach(img => {
-    let src = $(img).attr('src');
-    $(img).attr('data-src', src).removeAttr('src');
+  images.each((i, o) => {
+    let src = $(o).attr('src');
+    $(o).attr('data-src', src).removeAttr('src');
   });
 
   data.content = $.html();
