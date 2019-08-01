3e5bf1e66720cb3998d9c9efcdba6638cd24e542
ᴍɪᴍɪ <1119186082@qq.com>
2019-08-01 11:53:45 +0800

    Refactor lint / tests / bower (#1041)

--- source/js/scrollspy.js
+++ source/js/scrollspy.js
@@ -1,10 +1,10 @@
 /* ========================================================================
-* Bootstrap: scrollspy.js v3.3.2
-* http://getbootstrap.com/javascript/#scrollspy
-* ========================================================================
-* Copyright 2011-2015 Twitter, Inc.
-* Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-* ======================================================================== */
+ * Bootstrap: scrollspy.js v3.3.2
+ * http://getbootstrap.com/javascript/#scrollspy
+ * ========================================================================
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
 
 /**
  * Customized by iissnan & Ivan.Nginx
