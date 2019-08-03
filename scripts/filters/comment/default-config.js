ef9909718978cd0bfaea0ed7bdcff16cc93c0022
jiangtj <116749895@qq.com>
2019-08-03 16:11:34 +0800

    Fix QA

--- scripts/filters/comment/default-config.js
+++ scripts/filters/comment/default-config.js
@@ -9,7 +9,7 @@ hexo.extend.filter.register('theme_inject', function(injects) {
     let injectName = path.basename(element.name, path.extname(element.name));
     element.args[0] = Object.assign({
       class: injectName,
-      button: injectName,
+      button: injectName
     }, element.args[0]);
   });
 }, 99999);
