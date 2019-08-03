bfbaea68ffa90b2468fa73654f4c9e3d0ae3e15e
jiangtj <116749895@qq.com>
2019-08-03 17:33:33 +0800

    Add vk style

--- scripts/filters/comment/vkontakte.js
+++ scripts/filters/comment/vkontakte.js
@@ -9,6 +9,8 @@ hexo.extend.filter.register('theme_inject', injects => {
   let theme = hexo.theme.config;
   if (!theme.vkontakte_api.enable || !theme.vkontakte_api.comments) return;
 
-  injects.comment.raw('vkontakte-comments', '<div class="comments" id="comments"><div id="vk_comments"></div></div>', {}, {cache: true});
+  injects.comment.raw('vkontakte-comments', '<div class="comments" id="comments"><div id="vk_comments"></div></div>', {
+    button: '<i class="fa fa-vk" aria-hidden="true"></i> vkontakte'
+  }, {cache: true});
 
 }, priority.vkontakte_comments_plugin);
