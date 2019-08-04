49349d1a4de1d74df27fb92aec2d6419fe4e535c
MrTT <jiang.taojie@foxmail.com>
2019-08-04 16:11:45 +0800

    Add custom button support

--- scripts/filters/comment/default-config.js
+++ scripts/filters/comment/default-config.js
@@ -6,10 +6,17 @@ const path = require('path');
 
 hexo.extend.filter.register('theme_inject', injects => {
   injects.comment.raws.forEach(element => {
+    // Set default button content
     let injectName = path.basename(element.name, path.extname(element.name));
     element.args[0] = Object.assign({
       class: injectName,
       button: injectName
     }, element.args[0]);
+    // Set custom button content
+    let locals = element.args[0];
+    let customButton = hexo.theme.config.comments.button[locals.class];
+    if (customButton) {
+      locals.button = customButton;
+    }
   });
 }, 99999);
