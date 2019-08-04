b2b715707aee34a0c0637de6abe39b5305cd6734
ᴍɪᴍɪ <1119186082@qq.com>
2019-08-04 23:48:12 +0800

    Improve the style of firebase analytics and post-meta (#1056)

--- scripts/filters/comment/common.js
+++ scripts/filters/comment/common.js
@@ -5,8 +5,6 @@ module.exports = {
     <span class="post-meta-item-icon">
       <i class="fa fa-comment-o"></i>
     </span>
-    {% if theme.post_meta.item_text %}
-      <span class="post-meta-item-text">{{ __('post.comments_count') + __('symbol.colon') }}</span>
-    {% endif %}
+    <span class="post-meta-item-text">{{ __('post.comments_count') + __('symbol.colon') }}</span>
   `
 };
