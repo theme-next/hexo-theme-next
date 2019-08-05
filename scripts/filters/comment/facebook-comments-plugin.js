2fdefaa0d02d2bd3ac4a9f27873a18cbaf85f9ac
jiangtj <116749895@qq.com>
2019-08-05 12:50:49 +0800

    Add title

--- scripts/filters/comment/facebook-comments-plugin.js
+++ scripts/filters/comment/facebook-comments-plugin.js
@@ -34,7 +34,7 @@ hexo.extend.filter.register('theme_inject', injects => {
   {% if post.comments %}
   <span class="post-meta-item">
     ${iconText('comment-o', 'facebook')}
-    <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
+    <a title="facebook comments" href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
       <span class="post-comments-count fb-comments-count" data-href="{{ post.permalink }}" itemprop="commentCount">0</span>
     </a>
   </span>
