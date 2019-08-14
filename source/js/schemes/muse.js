d56aef37a9146842a94844794490c88d7eeb2a04
StevenJoeZhang <1119186082@qq.com>
2019-08-14 12:53:40 +0800

    Fix

--- source/js/schemes/muse.js
+++ source/js/schemes/muse.js
@@ -124,8 +124,8 @@ window.addEventListener('DOMContentLoaded', () => {
     mouseupHandler: function(event) {
       var deltaX = event.pageX - mousePos.X;
       var deltaY = event.pageY - mousePos.Y;
-      var clickingBlankPart = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY)) < 20 && $(e.target).is('.main');
-      if (this.isSidebarVisible && (clickingBlankPart || $(e.target).is('img.medium-zoom-image, .fancybox img'))) {
+      var clickingBlankPart = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY)) < 20 && $(event.target).is('.main');
+      if (this.isSidebarVisible && (clickingBlankPart || $(event.target).is('img.medium-zoom-image, .fancybox img'))) {
         this.hideSidebar();
       }
     },
