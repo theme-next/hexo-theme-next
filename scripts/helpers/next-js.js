5502357a25dababd7aa600e108c1b2ffa6df056a
jiangtj <116749895@qq.com>
2019-08-16 09:43:53 +0800

    Fix QA

--- scripts/helpers/next-js.js
+++ scripts/helpers/next-js.js
@@ -5,5 +5,5 @@
 hexo.extend.helper.register('next_js', function(...urls) {
   let js = hexo.theme.config.js;
   let version = this.next_env('version');
-  return urls.map((url) => this.js(`${js}/${url}?v=${version}`)).join('');
+  return urls.map(url => this.js(`${js}/${url}?v=${version}`)).join('');
 });
