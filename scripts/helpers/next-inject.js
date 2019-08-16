8ffc4892649dab69ccce15f112f72e3a9f95358c
jiangtj <116749895@qq.com>
2019-08-16 10:03:04 +0800

    Add next_inject helper

---
+++ scripts/helpers/next-inject.js
@@ -0,0 +1,9 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.helper.register('next_inject', function(point) {
+  return hexo.theme.config.injects[point]
+    .map(item => this.partial(item.layout, item.locals, item.options))
+    .join('');
+});
