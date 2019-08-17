f7b98743dbd73ae56af9be90c82d964c9b2fb0c8
1v9 <mkb@live.cn>
2019-08-17 11:39:09 +0800

    fix

--- scripts/helpers/next-vendors.js
+++ scripts/helpers/next-vendors.js
@@ -4,6 +4,6 @@
 
 hexo.extend.helper.register('next_vendors', function(url) {
   if (url.startsWith('//')) return url;
-  let internal = hexo.theme.config._internal;
+  let internal = hexo.theme.config.vendors._internal;
   return this.url_for(`${internal}/${url}`);
 });
