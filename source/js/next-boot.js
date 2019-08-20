aca3628a5bd9ed117586853b34141a1e884041cd
StevenJoeZhang <1119186082@qq.com>
2019-08-21 01:36:27 +0800

    Update

--- source/js/next-boot.js
+++ source/js/next-boot.js
@@ -1,6 +1,8 @@
 /* global NexT, CONFIG */
 
-window.addEventListener('DOMContentLoaded', () => {
+NexT.boot = {};
+
+NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
@@ -18,48 +20,8 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  window.addEventListener('hashchange', () => {
-    var tHash = location.hash;
-    if (tHash !== '' && !tHash.match(/%\S{2}/)) {
-      var target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
-      target && target.click();
-    }
-  });
-
-  // Define Motion Sequence & Bootstrap Motion.
-  if (CONFIG.motion.enable) {
-    NexT.motion.integrator
-      .add(NexT.motion.middleWares.logo)
-      .add(NexT.motion.middleWares.menu)
-      .add(NexT.motion.middleWares.postList)
-      .add(NexT.motion.middleWares.sidebar)
-      .bootstrap();
-  } else {
-    NexT.utils.updateSidebarPosition();
-  }
-});
-
-$(document).on('DOMContentLoaded pjax:success', () => {
-
-  /**
-   * Register JS handlers by condition option.
-   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
-   */
-  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  CONFIG.mediumzoom && window.mediumZoom('.post-body img');
-  CONFIG.lazyload && window.lozad('.post-body img').observe();
-  CONFIG.pangu && window.pangu.spacingPage();
-
-  CONFIG.exturl && NexT.utils.registerExtURL();
-  CONFIG.copycode.enable && NexT.utils.registerCopyCode();
-  NexT.utils.registerTabsTag();
-  NexT.utils.registerActiveMenuItem();
-  NexT.utils.embeddedVideoTransformer();
-
-  var sidebarNav = document.querySelector('.sidebar-nav');
   var TAB_ANIMATE_DURATION = 200;
-
-  $(sidebarNav.querySelectorAll('li')).on('click', event => {
+  $('.sidebar-nav li').on('click', event => {
     var item = $(event.currentTarget);
     var activeTabClassName = 'sidebar-nav-active';
     var activePanelClassName = 'sidebar-panel-active';
@@ -82,15 +44,6 @@ $(document).on('DOMContentLoaded pjax:success', () => {
     item.siblings().removeClass(activeTabClassName);
     item.addClass(activeTabClassName);
   });
-  if (document.querySelector('.post-toc-wrap').childElementCount > 0) {
-    sidebarNav.style.display = '';
-    sidebarNav.classList.add('motion-element');
-    document.querySelector('.sidebar-nav-toc').click();
-  } else {
-    sidebarNav.style.display = 'none';
-    sidebarNav.classList.remove('motion-element');
-    document.querySelector('.sidebar-nav-overview').click();
-  }
 
   /**
    * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
@@ -104,8 +57,63 @@ $(document).on('DOMContentLoaded pjax:success', () => {
   initSidebarDimension();
   window.addEventListener('resize', initSidebarDimension);
 
-  function wrapTable() {
-    $('table').not('.gist table').wrap('<div class="table-container"></div>');
+  window.addEventListener('hashchange', () => {
+    var tHash = location.hash;
+    if (tHash !== '' && !tHash.match(/%\S{2}/)) {
+      var target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
+      target && target.click();
+    }
+  });
+};
+
+NexT.boot.refresh = function() {
+
+  /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
+  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
+  CONFIG.mediumzoom && window.mediumZoom('.post-body img');
+  CONFIG.lazyload && window.lozad('.post-body img').observe();
+  CONFIG.pangu && window.pangu.spacingPage();
+
+  CONFIG.exturl && NexT.utils.registerExtURL();
+  CONFIG.copycode.enable && NexT.utils.registerCopyCode();
+  NexT.utils.registerTabsTag();
+  NexT.utils.registerActiveMenuItem();
+  NexT.utils.embeddedVideoTransformer();
+
+  var sidebarNav = document.querySelector('.sidebar-nav');
+  if (document.querySelector('.post-toc-wrap').childElementCount > 0) {
+    sidebarNav.style.display = '';
+    sidebarNav.classList.add('motion-element');
+    document.querySelector('.sidebar-nav-toc').click();
+  } else {
+    sidebarNav.style.display = 'none';
+    sidebarNav.classList.remove('motion-element');
+    document.querySelector('.sidebar-nav-overview').click();
   }
-  wrapTable();
+
+  $('table').not('.gist table').wrap('<div class="table-container"></div>');
+};
+
+NexT.boot.motion = function() {
+  // Define Motion Sequence & Bootstrap Motion.
+  if (CONFIG.motion.enable) {
+    NexT.motion.integrator
+      .add(NexT.motion.middleWares.logo)
+      .add(NexT.motion.middleWares.menu)
+      .add(NexT.motion.middleWares.postList)
+      .add(NexT.motion.middleWares.sidebar)
+      .bootstrap();
+  } else {
+    NexT.utils.updateSidebarPosition();
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  NexT.boot.registerEvents();
+  NexT.boot.refresh();
+  NexT.boot.motion();
 });
+window.addEventListener('pjax:success', NexT.boot.refresh);
