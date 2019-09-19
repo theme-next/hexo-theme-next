006a4b0f7c94ad38e67af78c5492d810e8027bd3
SanKo <sankoshine@users.noreply.github.com>
2019-09-19 15:34:01 +0800

    Fix fancybox & medium-zoom for hexo asset_img tag (#1166)

--- source/js/next-boot.js
+++ source/js/next-boot.js
@@ -80,7 +80,7 @@ NexT.boot.refresh = function() {
    * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
    */
   CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  CONFIG.mediumzoom && window.mediumZoom('.post-body :not(a) > img');
+  CONFIG.mediumzoom && window.mediumZoom('.post-body :not(a) > img, .post-body > img');
   CONFIG.lazyload && window.lozad('.post-body img').observe();
   CONFIG.pangu && window.pangu.spacingPage();
 
