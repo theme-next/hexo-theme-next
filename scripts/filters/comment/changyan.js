479c20b645a48918c4e8f3476cbe51333c4be59c
StevenJoeZhang <1119186082@qq.com>
2019-10-10 18:33:18 +0800

    Update

--- scripts/filters/comment/changyan.js
+++ scripts/filters/comment/changyan.js
@@ -30,13 +30,13 @@ hexo.extend.filter.register('theme_inject', injects => {
   <span class="post-meta-item">
     ${iconText('comment-o', 'changyan')}
     {% if is_post() %}
-      <a title="changyan" href="{{ url_for(post.path) }}#SOHUCS" itemprop="discussionUrl">{#
-        #}<span id="changyan_count_unit" class="post-comments-count hc-comment-count" data-xid="{{ post.path }}" itemprop="commentCount"></span>{#
-      #}</a>
+      <a title="changyan" href="{{ url_for(post.path) }}#SOHUCS" itemprop="discussionUrl">
+        <span id="changyan_count_unit" class="post-comments-count hc-comment-count" data-xid="{{ post.path }}" itemprop="commentCount"></span>
+      </a>
     {% else %}
-      <a title="changyan" href="{{ url_for(post.path) }}#SOHUCS" itemprop="discussionUrl">{#
-        #}<span id="url::{{ post.permalink }}" class="cy_cmt_count" data-xid="{{ post.path }}" itemprop="commentCount"></span>{#
-      #}</a>
+      <a title="changyan" href="{{ url_for(post.path) }}#SOHUCS" itemprop="discussionUrl">
+        <span id="url::{{ post.permalink }}" class="cy_cmt_count" data-xid="{{ post.path }}" itemprop="commentCount"></span>
+      </a>
     {% endif %}
   </span>
   {% endif %}
