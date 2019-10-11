6240775860fb711a03f57f4c2a57fea24d8f8b9e
1v9 <mkb@live.cn>
2019-10-11 14:59:55 +0800

    Replace id with class name (#1209)

--- source/js/schemes/muse.js
+++ source/js/schemes/muse.js
@@ -117,11 +117,8 @@ window.addEventListener('DOMContentLoaded', () => {
   sidebarToggleMotion.init();
 
   function updateFooterPosition() {
-    var containerHeight = document.querySelector('.header').offsetHeight + document.querySelector('.main').offsetHeight;
-    var footer = document.getElementById('footer');
-    if (footer.classList.contains('footer-fixed')) {
-      containerHeight += footer.outerHeight(true);
-    }
+    var footer = document.querySelector('.footer');
+    var containerHeight = document.querySelector('.header').offsetHeight + document.querySelector('.main').offsetHeight + footer.offsetHeight;
     footer.classList.toggle('footer-fixed', containerHeight <= window.innerHeight);
   }
 
--- source/js/utils.js
+++ source/js/utils.js
@@ -1,13 +1,5 @@
 /* global NexT, CONFIG */
 
-HTMLElement.prototype.outerHeight = function(flag) {
-  var height = this.offsetHeight;
-  if (!flag) return height;
-  var style = window.getComputedStyle(this);
-  height += parseInt(style.marginTop, 10) + parseInt(style.marginBottom, 10);
-  return height;
-};
-
 HTMLElement.prototype.wrap = function(wrapper) {
   this.parentNode.insertBefore(wrapper, this);
   this.parentNode.removeChild(this);
@@ -355,7 +347,7 @@ NexT.utils = {
    */
   initSidebarDimension: function() {
     var sidebarNav = document.querySelector('.sidebar-nav');
-    var sidebarNavHeight = sidebarNav.style.display !== 'none' ? sidebarNav.outerHeight(true) : 0;
+    var sidebarNavHeight = sidebarNav.style.display !== 'none' ? sidebarNav.offsetHeight : 0;
     var sidebarOffset = CONFIG.sidebar.offset || 12;
     var sidebarb2tHeight = CONFIG.back2top.enable && CONFIG.back2top.sidebar ? document.querySelector('.back-to-top').offsetHeight : 0;
     var sidebarSchemePadding = CONFIG.sidebarPadding + sidebarNavHeight + sidebarb2tHeight;
