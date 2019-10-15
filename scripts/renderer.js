c831fe274a11a9a0d7e0726f01866fe610ed7db1
1v9 <mkb@live.cn>
2019-10-15 16:47:25 +0800

    log err

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -9,7 +9,7 @@ function njkCompile(data) {
   const templateDir = path.dirname(data.path);
   const config = Object.assign({
     autoescape: false,
-    watch: false
+    watch     : false
   }, hexo.config.nunjucks);
   const env = nunjucks.configure(templateDir, config);
   return nunjucks.compile(data.text, env, data.path);
@@ -24,9 +24,16 @@ njkRenderer.compile = function(data) {
   const compiledTemplate = njkCompile(data);
   // Need a closure to keep the compiled template.
   return function(locals, callback) {
-    return compiledTemplate.render(locals, callback);
-  }
-}
+    return new Promise(resolve => {
+      compiledTemplate.render(locals, callback, (err, result) => {
+        if (err) {
+          hexo.log.error(err);
+        }
+        resolve(result);
+      });
+    });
+  };
+};
 
 hexo.extend.renderer.register('njk', 'html', njkRenderer);
 hexo.extend.renderer.register('swig', 'html', njkRenderer);
