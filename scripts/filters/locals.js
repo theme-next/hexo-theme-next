9dd87ce09888466392b9ce86d25e1db7b8d9dad6
ᴍɪᴍɪ <1119186082@qq.com>
2019-10-17 16:58:38 +0800

    Migrate to Nunjucks [1] (#1218)

---
+++ scripts/filters/locals.js
@@ -0,0 +1,21 @@
+/* global hexo */
+
+'use strict';
+
+const path = require('path');
+
+hexo.extend.filter.register('template_locals', function(locals) {
+  const { config } = this;
+  // Hexo & NexT version
+  locals.hexo_version = this.env.version;
+  locals.next_version = require(path.normalize('../../package.json')).version;
+  // Language & Config
+  locals.title = locals.__('title') !== 'title' && locals.__('title') || config.title;
+  locals.subtitle = locals.__('subtitle') !== 'subtitle' && locals.__('subtitle') || config.subtitle;
+  locals.author = locals.__('author') !== 'author' && locals.__('author') || config.author;
+  locals.description = locals.__('description') !== 'description' && locals.__('description') || config.description;
+  // Current year
+  locals.copyright_year = new Date().getFullYear();
+  // PJAX
+  locals.pjax = config.theme.pjax ? ' pjax' : '';
+});
