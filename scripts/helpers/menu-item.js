18cc02a8c111a3f6fd8496eb0671074cc4b5eae8
Mimi <1119186082@qq.com>
2019-10-17 18:43:07 +0800

    Update

---
+++ scripts/helpers/menu-item.js
@@ -0,0 +1,31 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.helper.register('menu_items', () => {
+  var config = hexo.theme.config.font;
+
+  if (!config || !config.enable) {
+    return '';
+  }
+
+  var fontDisplay = '&display=swap';
+  var fontSubset = '&subset=latin,latin-ext';
+  var fontStyles = ':300,300italic,400,400italic,700,700italic';
+  var fontHost = config.host || '//fonts.googleapis.com';
+
+  //Get a font list from config
+  var fontFamilies = ['global', 'title', 'headings', 'posts', 'codes'].map(item => {
+    if (config[item] && config[item].family && config[item].external) {
+      return config[item].family + fontStyles;
+    }
+    return '';
+  });
+
+  fontFamilies = fontFamilies.filter(item => item !== '');
+  fontFamilies = [...new Set(fontFamilies)];
+  fontFamilies = fontFamilies.join('|');
+
+  // Merge extra parameters to the final processed font string
+  return fontFamilies ? `<link rel="stylesheet" href="${fontHost}/css?family=${fontFamilies.concat(fontDisplay, fontSubset)}">` : '';
+});
