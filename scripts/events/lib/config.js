0989b54a04274907e6edc1620f6c5e91f9c2eefd
ᴍɪᴍɪ <1119186082@qq.com>
2019-10-21 13:52:22 +0800

    Replace lodash with native API (#1230)

--- scripts/events/lib/config.js
+++ scripts/events/lib/config.js
@@ -1,12 +1,25 @@
 'use strict';
 
+function isObject(item) {
+  return item && typeof item === 'object' && !Array.isArray(item);
+}
+
+function merge(target, source) {
+  for (const key in source) {
+    if (isObject(target[key]) && isObject(source[key])) {
+      merge(target[key], source[key]);
+    } else {
+      target[key] = source[key];
+    }
+  }
+  return target;
+}
+
 module.exports = hexo => {
   if (!hexo.locals.get) return;
 
   var data = hexo.locals.get('data');
   if (!data) return;
-
-  const merge = require(hexo.base_dir + 'node_modules/lodash/merge');
 
   /**
    * Merge configs from _data/next.yml into hexo.theme.config.
