6c2ebce0212d0f77452eaa16da2e8820a2a07478
jiangtj <116749895@qq.com>
2019-11-21 11:46:35 +0800

    Set default extname from file's extname

--- scripts/events/lib/injects.js
+++ scripts/events/lib/injects.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const points = require('./injects-point');
+const defaultExtname = '.swig';
 
 // Defining stylus types
 class StylusInject {
@@ -23,9 +24,17 @@ class ViewInject {
     this.raws = [];
   }
   raw(name, raw, ...args) {
+    // Set default extname
+    if (path.extname(name) === '') {
+      name += defaultExtname;
+    }
     this.raws.push({name, raw, args});
   }
   file(name, file, ...args) {
+    // Set default extname from file's extname
+    if (path.extname(name) === '') {
+      name += path.extname(file);
+    }
     // Get absolute path base on hexo dir
     this.raw(name, fs.readFileSync(path.resolve(this.base_dir, file), 'utf8'), ...args);
   }
@@ -58,13 +67,9 @@ module.exports = hexo => {
   points.views.forEach(type => {
     let configs = Object.create(null);
     hexo.theme.config.injects[type] = [];
+    // Add or override view.
     injects[type].raws.forEach((injectObj, index) => {
-      // If there is no suffix, `.swig` will be added
-      if (injectObj.name.indexOf('.') < 0) {
-        injectObj.name += '.swig';
-      }
       let name = `inject/${type}/${injectObj.name}`;
-      // Add or override view.
       hexo.theme.setView(name, injectObj.raw);
       configs[name] = {
         layout : name,
@@ -73,6 +78,7 @@ module.exports = hexo => {
         order  : injectObj.args[2] || index
       };
     });
+    // Views sort.
     hexo.theme.config.injects[type] = Object.values(configs)
       .sort((x, y) => x.order - y.order);
   });
