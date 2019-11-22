14172486dfb480aa80d467592963f57cc0bb734d
Mimi <1119186082@qq.com>
2019-11-22 17:53:41 +0800

    Add post navigation display configuration

--- scripts/helpers/engine.js
+++ scripts/helpers/engine.js
@@ -30,6 +30,26 @@ hexo.extend.helper.register('post_edit', function(src) {
   });
 });
 
+hexo.extend.helper.register('post_nav', function(post) {
+  const theme = hexo.theme.config;
+  if (theme.post_navigation === false || (!post.prev && !post.next)) return '';
+  const prev = theme.post_navigation === 'right' ? post.prev : post.next;
+  const next = theme.post_navigation === 'right' ? post.next : post.prev;
+  const left = prev ? `
+    <a href="${this.url_for(prev.path)}" rel="prev" title="${prev.title}">
+      <i class="fa fa-chevron-left"></i> ${prev.title}
+    </a>` : '';
+  const right = next ? `
+    <a href="${this.url_for(next.path)}" rel="next" title="${next.title}">
+      ${next.title} <i class="fa fa-chevron-right"></i>
+    </a>` : '';
+  return `
+    <div class="post-nav">
+      <div class="post-nav-item">${left}</div>
+      <div class="post-nav-item">${right}</div>
+    </div>`
+});
+
 hexo.extend.helper.register('gitalk_md5', function(path) {
   let str = this.url_for(path);
   str = encodeURI(str);
