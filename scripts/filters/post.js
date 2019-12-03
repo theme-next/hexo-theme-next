ba019030a1293c80536fcb16777cc05a94305118
ᴍɪᴍɪ <1119186082@qq.com>
2019-12-03 20:48:05 +0800

    Drop cheerio (#1299)

--- scripts/filters/post.js
+++ scripts/filters/post.js
@@ -10,40 +10,23 @@ hexo.extend.filter.register('after_post_render', data => {
     lazyload: theme.lazyload
   };
   if (!filters.exturl && !filters.lazyload) return;
-  const cheerio = require('cheerio');
-  const $ = cheerio.load(data.content, {
-    decodeEntities: false
-  });
   if (filters.lazyload) {
-    $('img').each((i, o) => {
-      let src = $(o).attr('src');
-      $(o).attr('data-src', src).removeAttr('src');
-    });
+    data.content = data.content.replace(/(<img[^>]*) src=/img, '$1 data-src=');
   }
   if (filters.exturl) {
     const url = require('url');
     var siteHost = url.parse(config.url).hostname || config.url;
-
-    $('a').each((i, o) => {
-      var href = $(o).attr('href');
+    data.content = data.content.replace(/<a[^>]* href="([^"]+)"[^>]*>([^<]+)<\/a>/img, (match, href, html) => {
       // Exit if the href attribute doesn't exists.
-      if (!href) return;
+      if (!href) return match;
 
-      var data = url.parse(href);
-
-      // Exit if the url has same host with `config.url`, which means it's an internal link
-      if (!data.protocol || data.hostname === siteHost) return;
+      // Exit if the url has same host with `config.url`, which means it's an internal link.
+      var link = url.parse(href);
+      if (!link.protocol || link.hostname === siteHost) return match;
 
       // If title atribute filled, set it as title; if not, set url as title.
-      var title = $(o).attr('title') || href;
-
-      var encoded = Buffer.from(href).toString('base64');
-
-      $(o).replaceWith(() => {
-        return $(`<span class="exturl" data-url="${encoded}" title="${title}">${$(o).html()}<i class="fa fa-external-link"></i></span>`);
-      });
+      return `<span class="exturl" data-url="${Buffer.from(href).toString('base64')}" title="${href}">${html}<i class="fa fa-external-link"></i></span>`;
     });
   }
-  data.content = $.html();
 
-}, 20);
+}, 0);
