6984cdddb0528a4d4ba9065ed44da8cf5e020bf0
MrTT <jiang.taojie@foxmail.com>
2020-01-12 23:43:20 +0800

    ...

--- flamegraph.sh
+++ flamegraph.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-ls -al
+pwd
 cd ..
-ls -al
-hexo init temp
+npm install -g hexo
+npm install -g 0x
+mv repo next
+hexo init repo
+cd repo
+mv ../next themes/next
+hexo config theme next
+0x --output-dir 'dist' -- node ./node_modules/.bin/hexo g
