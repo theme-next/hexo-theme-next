9bebf7acd0f69f98ef8be56b890da828dc2df02e
MrTT <jiang.taojie@foxmail.com>
2020-01-12 23:51:15 +0800

    Add posts

--- flamegraph.sh
+++ flamegraph.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-pwd
+# Create hexo project
+echo "- Create hexo project"
 cd ..
 npm install -g hexo
 npm install -g 0x
@@ -9,4 +10,12 @@ hexo init repo
 cd repo
 mv ../next themes/next
 hexo config theme next
+
+# Clone data
+echo "- Import 300 posts"
+git clone https://github.com/SukkaLab/hexo-many-posts.git source/_posts/hexo-many-posts --depth=1 --quiet
+
+# Exec 0x
+echo "- Generating flamegraph..."
 0x --output-dir 'dist' -- node ./node_modules/.bin/hexo g
+mv dist/flamegraph.html dist/index.html
