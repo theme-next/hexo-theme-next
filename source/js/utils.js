fd5927489ea36726cb70d1e30294ed97c047a4be
Mimi <1119186082@qq.com>
2020-03-03 11:07:26 +0800

    Optimize the loading of exturl

--- source/js/utils.js
+++ source/js/utils.js
@@ -45,12 +45,13 @@ NexT.utils = {
 
   registerExtURL: function() {
     document.querySelectorAll('.exturl').forEach(element => {
-      element.addEventListener('click', event => {
-        var exturl = event.currentTarget.dataset.url;
-        var decurl = decodeURIComponent(escape(window.atob(exturl)));
-        window.open(decurl, '_blank', 'noopener');
-        return false;
-      });
+      let link = document.createElement('a');
+      let exturl = element.dataset.url;
+      link.href = decodeURIComponent(escape(window.atob(exturl)));
+      link.rel = 'noopener external nofollow noreferrer';
+      link.target = '_blank';
+      link.innerHTML = element.innerHTML;
+      element.parentNode.replaceChild(link, element);
     });
   },
 
