ec521c927dc10255977324284c1c667f2e220da7
jiangtj <116749895@qq.com>
2020-08-03 11:17:05 +0800

    fix: Chinese TOC cannot jump

--- source/js/utils.js
+++ source/js/utils.js
@@ -235,10 +235,10 @@ NexT.utils = {
     const navItems = document.querySelectorAll('.post-toc li');
     const sections = [...navItems].map(element => {
       var link = element.querySelector('a.nav-link');
+      var target = document.getElementById(decodeURI(link.getAttribute('href')).replace('#', ''));
       // TOC item animation navigate.
       link.addEventListener('click', event => {
         event.preventDefault();
-        var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
           targets  : document.scrollingElement,
@@ -247,7 +247,7 @@ NexT.utils = {
           scrollTop: offset + 10
         });
       });
-      return document.getElementById(link.getAttribute('href').replace('#', ''));
+      return target;
     });
 
     var tocElement = document.querySelector('.post-toc-wrap');
